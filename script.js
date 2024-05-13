ecaa826e06ce5e7f6b2f451bb053d082d5248deb
TaqiyyaSedantri <taqiyya1001@gmail.com>
2024-05-13 16:38:28 +0700

    fisrt commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// Animasi muncul saat scroll
+window.addEventListener('scroll', function() {
+    var elementsToShow = document.querySelectorAll('.show-on-scroll');
+    elementsToShow.forEach(function(element) {
+        if (isElementInViewport(element)) {
+            element.classList.add('is-visible');
+        } else {
+            element.classList.remove('is-visible');
+        }
+    });
+});
+
+function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
